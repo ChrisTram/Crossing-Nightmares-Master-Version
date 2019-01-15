00919e2727b1ecfc4a8474ae222929f506417948
Chris <chris.tramier97@gmail.com>
2019-01-15 09:31:07 +0100

    init commit

---
+++ Assets/Scripts/EnnemyController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class EnnemyController : MonoBehaviour
+{
+
+
+    public NavMeshAgent agent;
+
+    public ThirdPersonCharacter character;
+
+    public ThirdPersonCharacter player;
+
+    void Start()
+    {
+        agent.updateRotation = false;
+    }
+
+    void Update()
+    {
+        agent.SetDestination(player.transform.position);
+        if (agent.remainingDistance > agent.stoppingDistance)
+        {
+            character.Move(agent.desiredVelocity, false, false);
+        }
+        else
+        {
+            character.Move(Vector3.zero, false, false);
+        }
+    }
+}
