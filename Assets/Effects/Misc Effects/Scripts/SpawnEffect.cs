a545bc82e4c015ff9ad063d6fc11f1ab5d4ae8eb
Chris <chris.tramier97@gmail.com>
2019-02-25 16:09:17 +0100

    Added a Script to handle the Respawn effect

---
+++ Assets/Effects/Misc Effects/Scripts/SpawnEffect.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEffect : MonoBehaviour
+{
+
+    public float spawnEffectTime = 2;
+    public float pause = 1;
+    public AnimationCurve fadeIn;
+
+    public Material materialRespawn;
+    public Material materialFinal;
+
+    ParticleSystem ps;
+    float timer = 0;
+    Renderer _renderer;
+
+    int shaderProperty;
+
+    void Start()
+    {
+        shaderProperty = Shader.PropertyToID("_cutoff");
+        _renderer = GetComponent<Renderer>();
+        ps = GetComponentInChildren<ParticleSystem>();
+        _renderer.sharedMaterial = materialRespawn;
+        var main = ps.main;
+        main.duration = spawnEffectTime;
+
+        ps.Play();
+
+    }
+
+    void Update()
+    {
+        if (timer < spawnEffectTime + pause)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            //Fin de l'animation de Spawn
+        }
+        if (!(timer < spawnEffectTime / 4 + pause))
+        {
+            _renderer.sharedMaterial = materialFinal; //A partir de ce moment on peut rendre l'apparence basique du personnage
+        }
+
+        _renderer.material.SetFloat(shaderProperty, fadeIn.Evaluate(Mathf.InverseLerp(0, spawnEffectTime, timer)));
+
+    }
+}
