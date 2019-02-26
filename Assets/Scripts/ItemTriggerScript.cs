f9ebfa5c07b54bad30e180ac05994f46931d2d75
Chris <chris.tramier97@gmail.com>
2019-02-26 10:51:16 +0100

    Added ItemTriggerScript

---
+++ Assets/Scripts/ItemTriggerScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemTriggerScript : MonoBehaviour {
+
+    public bool isDamaging;
+    public float damage = 1;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            other.SendMessage((isDamaging) ? "TakeDamage" : "HealDamage", damage);
+        Destroy(gameObject);
+    }
+}
