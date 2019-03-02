0b21ba979e9946e1bd4070b8cf3a05f181f205f2
Chris <chris.tramier97@gmail.com>
2019-03-02 11:56:42 +0100

    minor corrections

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.AI;
 using UnityStandardAssets.Characters.ThirdPerson;
 
-public class EnnemyController : MonoBehaviour
+public class EnemyController : MonoBehaviour
 {
 
 
