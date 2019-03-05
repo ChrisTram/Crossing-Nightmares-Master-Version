e56f66d55d7ea509aec005e7ae4cd81cbb426181
Chris <chris.tramier97@gmail.com>
2019-03-05 11:54:01 +0100

    Added TMPRO. Testing some Title UI

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+    public GameObject UITitle;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            UITitle.SetActive(true);
+        }
+    }
+}
