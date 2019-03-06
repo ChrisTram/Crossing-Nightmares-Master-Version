eefd37be5fd060b681c41f39810316fcdead2fce
Chris <chris.tramier97@gmail.com>
2019-03-06 16:44:35 +0100

    UI Slots & Item scripts

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+
+    public string name;
+    public int slotNb = -1; //-1 Par défaut, n'est donc pas dans un slot UI
+    public Sprite sprite; //Le sprite affiché dans l'UI
+
+    public Item(string itemName, Sprite spriteRef)
+    {
+        name = itemName;
+        sprite = spriteRef;
+    }
+
+}
---
+++ Assets/Scripts/UISlots.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+
+public class UISlots : MonoBehaviour {
+
+    //[SerializeField] List<Sprite> spriteList;
+    [SerializeField] List<GameObject> slots;
+    private int freeSlot = 0;
+    private Vector3 autoLocalScale = new Vector3( 1.0f, 1.0f, 1.0f );
+
+    public void AddItemInSlot(Item item)
+    {
+        if (item == null)
+            return;
+
+        GameObject NewSlot = new GameObject(); //Create the GameObject
+        Image NewImage = NewSlot.AddComponent<Image>(); //Add the Image Component script
+        NewImage.name = "UI" + item.name;
+        NewImage.sprite = item.sprite; //Set the Sprite of the Image Component on the new GameObject
+        NewSlot.GetComponent<RectTransform>().SetParent(this.transform); //Assign the newly created Image GameObject as a Child of the Parent Panel.
+        NewSlot.SetActive(true); //Activate the GameObject
+        slots.Add(NewSlot);
+
+
+        /*
+        Image newSlot = new Image();
+        slots[freeSlot].GetComponent<Image>().sprite = item.sprite;
+        slots[free]
+        item.slotNb = freeSlot; 
+        freeSlot++;*/
+    }
+
+    public void RemoveItemInSlot(Item itemToRemove)
+    {
+        //Find the item
+        GameObject slotToRemove = slots.Where(obj => obj.name == "UI"+itemToRemove.name).SingleOrDefault();
+        Destroy(slotToRemove);
+
+        //Remove the item
+        //remove 1 from slotNb for all the item after itemToRemove.slotNb
+    }
+    public void RemoveAllItemInSlot(Item itemToRemove)
+    {
+        //Same, but removing all items (se .ToList() instead of SingleOrDefault() and that will return a collection of all objects named "Sword")
+    }
+}
