0350c2855d822069ed92172197b9d8673860c3ef
Chris <chris.tramier97@gmail.com>
2019-03-18 14:36:36 +0100

    Canvas and UIManager.cs edit to handle itemInfo

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,16 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class UIManager : MonoBehaviour {
+public class UIManager : MonoBehaviour
+{
 
+    public static UIManager instance;
     public GameObject UITitle;
+    public GameObject UIItemInfo;
+    /*
+    public TMP_Text UITitleText;
+    public TMPro UIItemInfoText;
+    */
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
 
+    }
+
+    public void showTitle()
+    {
+        UITitle.SetActive(true);
+        StartCoroutine(RemoveAfterSeconds(3));
+
+    }
+    IEnumerator RemoveAfterSeconds(int seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        UITitle.SetActive(false);
+    }
+
+    public void showItemInfo(string itemName)
+    {
+        UIItemInfo.GetComponentInChildren<TMP_Text>().text = itemName;
+        UIItemInfo.SetActive(true);
+    }
+
+    public void disableItemInfo()
+    {
+        UIItemInfo.SetActive(true);
+    }
+}
+        /*
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
             UITitle.SetActive(true);
         }
-    }
-}
+    }*/
+
