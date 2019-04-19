bd10a55c1584aa3db55dc81d9bae655990ff6167
Chris <chris.tramier97@gmail.com>
2019-04-19 18:51:24 +0200

    Main Menu basics interactions and animations

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+    public static MainMenu instance;
+	public Animator loadGamePanel;
+    public Animator settingsPanel;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    void Start () {
+        CloseLoadGamePanel();
+        CloseSettingsPanel();
+	}
+	
+	public void ClickLoadGame()
+    {
+        loadGamePanel.gameObject.SetActive(true);
+        loadGamePanel.SetTrigger("activate");
+
+        if (settingsPanel.gameObject.activeInHierarchy)
+            settingsPanel.SetTrigger("desactivate");
+    }
+
+    public void ClickSettings()
+    {
+        settingsPanel.gameObject.SetActive(true);
+        settingsPanel.SetTrigger("activate");
+
+        if (loadGamePanel.gameObject.activeInHierarchy)
+            loadGamePanel.SetTrigger("desactivate");
+    }
+
+    public void CloseLoadGamePanel()
+    {
+        loadGamePanel.gameObject.SetActive(false);
+
+    }
+
+    public void CloseSettingsPanel()
+    {
+        settingsPanel.gameObject.SetActive(false);
+
+    }
+}
---
+++ Assets/Scripts/MainMenuEvents.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuEvents : MonoBehaviour {
+
+	public void ClosePanel()
+    {
+        if (name == "LoadGamePanel")
+            MainMenu.instance.CloseLoadGamePanel();
+        else
+            MainMenu.instance.CloseSettingsPanel();
+    }
+}
