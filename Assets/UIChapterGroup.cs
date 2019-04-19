572c769b388f2c82a6c8d153ec7605a2f2bc7d8c
Chris <chris.tramier97@gmail.com>
2019-04-19 16:38:15 +0200

    Added ChapterSelect screen with a script to adjust their sizes

---
+++ Assets/UIChapterGroup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIChapterGroup : MonoBehaviour {
+
+    public float width;
+    public float height;
+
+
+    void Start()
+    {
+        width = this.gameObject.GetComponent<RectTransform>().rect.width;
+        height = this.gameObject.GetComponent<RectTransform>().rect.height;
+
+        Vector2 newSize = new Vector2((width / 4)-20, (height / 5)-20);
+        this.gameObject.GetComponent<GridLayoutGroup>().cellSize = newSize;
+    }
+}
