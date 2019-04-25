e7a1188722ab4b348ed53f6aa2c7e98518e6cfc2
Chris <chris.tramier97@gmail.com>
2019-04-25 15:55:55 +0200

    Added auto gen of chapter select button

---
+++ Assets/Scripts/UIChapterNumber.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIChapterNumber : MonoBehaviour {
+
+    void Start()
+    {
+        TMP_Text text = GetComponent<TMP_Text>();
+        text.text = gameObject.transform.parent.name;
+    }
+}
