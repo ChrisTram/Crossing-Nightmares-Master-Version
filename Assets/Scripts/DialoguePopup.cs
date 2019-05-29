ba1f6ae77e8ed2def4bd8b6437ecd5523c05b36a
Chris <chris.tramier97@gmail.com>
2019-05-29 10:59:54 +0200

    DialoguePopup Script first draft

--- Assets/Scripts/DialoguePopup.cs
+++ Assets/Scripts/DialoguePopup.cs
@@ -1,16 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class DialoguePopup : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	public static DialoguePopup Create(Vector3 position, string dialogue)
+    {
+        Transform dialoguePopupTransform = Instantiate(Resources.Load<Transform>("pfDialoguePopup"), position, Quaternion.identity);
+
+        DialoguePopup dialoguePopup = dialoguePopupTransform.GetComponent<DialoguePopup>();
+        dialoguePopup.Setup(dialogue);
+
+        return dialoguePopup;
+    }
+
+    private TextMeshPro textMesh;
+    private float disappearTimer;
+    private Color textColor;
+
+    private void Awake()
+    {
+        textMesh = transform.GetComponent<TextMeshPro>();
+    }
+
+    public void Setup(string dialogueToLoc)
+    {
+        //textMesh.SetText(LocalizationManager.instance.GetLocalizedValue(dialogueToLoc));
+        textMesh.SetText(dialogueToLoc);
+        textColor = textMesh.color;
+        disappearTimer = 1f;
+    }
+
+    private void Update()
+    {
+        float moveYSpeed = 20f;
+        transform.position += new Vector3(0, moveYSpeed) * Time.deltaTime;
+
+        disappearTimer -= Time.deltaTime;
+        if (disappearTimer < 0)
+        {
+            // Start disappearing
+            float disappearSpeed = 2f;
+            textColor.a -= disappearSpeed * Time.deltaTime;
+            textMesh.color = textColor;
+            if (textColor.a < 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
 }
