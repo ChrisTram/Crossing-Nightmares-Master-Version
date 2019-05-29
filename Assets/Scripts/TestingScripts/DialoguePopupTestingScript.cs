2dc8224bfadab22e4772739966131729e18ff1f4
Chris <chris.tramier97@gmail.com>
2019-05-29 10:59:23 +0200

    Moved my testingScript

---
+++ Assets/Scripts/TestingScripts/DialoguePopupTestingScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialoguePopupTestingScript : MonoBehaviour {
+
+    // Use this for initialization
+    public Camera cam;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                DialoguePopup.Create(hit.point,"Hey, Listen...");
+            }
+        }
+	}
+}
