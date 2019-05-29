b630d008700d77df3d614b688bbc2f196552786e
Chris <chris.tramier97@gmail.com>
2019-05-29 10:07:22 +0200

    Created GameAssets script

---
+++ Assets/Scripts/GameAssets.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameAssets : MonoBehaviour {
+
+    public static GameAssets _i;
+
+    public static GameAssets i
+    {
+        get {
+            if (_i == null) _i = Instantiate(Resources.Load<GameAssets>("GameAssets"));
+            return _i;
+        }
+    }
+
+    public Transform pfDialoguePopup;
+}
