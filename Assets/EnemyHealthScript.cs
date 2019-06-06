6e611cb8814ec0b96e5a46cd1ad76899540210d6
Chris <chris.tramier97@gmail.com>
2019-06-06 11:26:42 +0200

    Enemies Health Script

---
+++ Assets/EnemyHealthScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthScript : MonoBehaviour {
+
+    public float health = 100;
+    public float maxHP = 100;
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        if (health < 0)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
