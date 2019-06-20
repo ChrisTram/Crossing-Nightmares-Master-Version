2f2129ccf94b653faef11fce480f925d7175d05a
Chris <chris.tramier97@gmail.com>
2019-06-20 11:08:49 +0200

    Minor correction & added loots for triggers

--- Assets/Scripts/TriggerScript.cs
+++ Assets/Scripts/TriggerScript.cs
@@ -5,13 +5,21 @@ using System.Linq;
 public class TriggerScript : MonoBehaviour {
     public string itemToTriggerStr;
     public string key_Name;
-
+    public bool giveAnotherItem = false;
+    public Transform itemToDrop;
 
     public void Trigger()
     {
         if(UISlotsManager.instance.checkItem(itemToTriggerStr))
         {
             UISlotsManager.instance.RemoveItemInSlot(new Item(itemToTriggerStr));
+            //On vérifie si le trigger doit drop un objet
+            if(giveAnotherItem)
+            {
+                Debug.Log("Drop Item");
+                Instantiate(itemToDrop, gameObject.transform.position, Quaternion.identity);
+            }
+            
             Destroy(gameObject);
         }
         
